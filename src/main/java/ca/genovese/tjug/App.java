df0936527179aacb87966399f850d3c3b8739cb1
Angelo Genovese <angelo@streetcontxt.com>
2018-07-26 18:52:20 -0400

    Initial Setup

---
+++ src/main/java/ca/genovese/tjug/App.java
@@ -0,0 +1,13 @@
+package ca.genovese.tjug;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
